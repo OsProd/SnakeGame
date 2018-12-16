dfb82a4683b4864b19f9391d50be5bf887678af5
OsProd <osman148opop@gmail.com>
2018-12-16 18:04:26 +0500

    Revert "123"
    This reverts commit ab061ad5e3561c9bfe041de41a1ebf8438fb4af9.

--- Snake/Assets/Scripts/UIButtons.cs
+++ Snake/Assets/Scripts/UIButtons.cs
@@ -3,20 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class UIButtons : MonoBehaviour {
     public static bool renderLine;
-
-    private void Start()
-    {
-        if (camController.maxScore > 0)
-        {
-            renderLine = true;
-        }
-    }
     public void Restart()
     {
-
+        renderLine = true;
         SceneManager.LoadScene("SampleScene");
     }
 }
--- Snake/Assets/Scripts/highScore.cs
+++ Snake/Assets/Scripts/highScore.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class highScore : MonoBehaviour {
-    public GameObject highScoreLine;
-    public GameObject text;
-    public Transform canvas;
+    private LineRenderer highLine;
 
-
+    private void Start()
+    {
+        highLine = GetComponent<LineRenderer>();
+    }
     void Update () {
         if (UIButtons.renderLine)
         {
-            Instantiate(highScoreLine, new Vector3(0, camController.maxScore, 0), Quaternion.identity);
-            Instantiate(text, new Vector3 (0, camController.maxScore, 0), Quaternion.identity, canvas);
+            transform.position = new Vector2(transform.position.x, camController.maxScore);
+            highLine.SetPosition(0, new Vector3(highLine.GetPosition(0).x, camController.maxScore, highLine.GetPosition(0).z));
+            highLine.SetPosition(1, new Vector3(highLine.GetPosition(1).x, camController.maxScore, highLine.GetPosition(1).z));
             UIButtons.renderLine = false;
         }
     }
